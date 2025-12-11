e28af003b89c9e4a3db7fcf1ddc8942f711b7620
AstridVides-cyber <vkirito67@gmail.com>
2025-12-11 02:13:41 -0600

    helloWorld

---
+++ helloWorld.cpp
@@ -0,0 +1,29 @@
+// Hacer mi primer hola mundo en C++
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hola mundo" << endl;
+    return 0;
+
+};
+
+/*
+- #include es una directiva del preprocesador que le dice al compilador
+- <iostream> es una biblioteca que define:
+    - cout (salida a consola)
+    - cin (entrada desde la consola)
+    - endl (fin de linea)
+- using es espacio de nombres
+- std es el espacio de nombres donde están todas las funciones estándar de C++
+- int es un tipo de dato que indica que la función main devuelve un número entero
+- main es la función principal
+- () es para poner parámetros
+- {} contiene las instrucciones del programa
+- endl termina/finaliza el cout (y agrega un salto de línea)
+- \n es un salto de línea
+- return 0 indica al sistema operativo que el programa terminó exitosamente (0 = sin errores)
+*/
+
+
+
