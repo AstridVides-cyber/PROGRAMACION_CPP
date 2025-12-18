0dcb76a52d4ae9909d1742d16118ca4c4b6e177e
AstridVides-cyber <vkirito67@gmail.com>
2025-12-18 03:46:48 -0600

    add retiro de dinero

---
+++ retiroDinero.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //int -> Variables de tipo entero
+    //double, float -> Variables de tipo decimal
+    //string -> variables que guardan cadenas de texto
+    //char -> variables que guardan un solo caracter
+    //bool -> variables que guardan valores logicos (true o false)
+    int cantidad, total = 1000;
+    cout << "Ingrese la cantidad de dinero a retirar: ";
+    cin >> cantidad;
+
+    if(cantidad <= total) {
+        total -= cantidad; // equivalente a total = total - cantidad
+        cout << "Retiro exitoso. Su nuevo saldo es: " << total << endl;
+    } else {
+        cout << "Saldo insuficiente." << endl;
+    }
+
+    return 0;
+}
