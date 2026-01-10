0ece78dcb9b7f903658d5357901e8ad5034f25ac
AstridVides-cyber <vkirito67@gmail.com>
2026-01-10 01:58:36 -0600

    funciones

---
+++ funciones.cpp
@@ -0,0 +1,49 @@
+// Uso de funciones
+#include <iostream>
+using namespace std;
+
+/*
+// Ejemplo 1
+// Estructura de una función en C++
+// Código que realiza una tarea específica
+int suma(int a, int b) {
+    return a + b; // Opcional, dependiendo del tipo de retorno
+}
+
+int main() {
+
+    int resultado = suma(3, 4); // Llamada a la función suma
+    cout << "El resultado es: " << resultado << endl;
+    
+    return 0;
+}
+*/
+
+/*
+// Ejemplo 2
+// Funciones con retorno
+int doble(int numero) {
+    return numero * 2;
+}
+
+int main() {
+
+    int valor = 5;
+    int resultado = doble(valor); // Llamada a la función doble
+    cout << "El doble de " << valor << " es " << resultado << endl;
+
+    return 0;
+}
+*/
+
+// Ejemplo 3
+// Funciones sin retorno
+void saludo() {
+    cout << "Hola!" << endl;
+}
+
+int main() {
+    saludo(); // Llamada a la función saludo
+
+    return 0;
+}
